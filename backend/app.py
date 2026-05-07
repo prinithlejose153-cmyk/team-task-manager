9aeb85ac6417af170fca07229d0fa74337a120ed
prinithlejose153-cmyk <prinithlejose.153@gmail.com>
2026-05-07 17:15:16 +0530

    added permanent demo users

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from backend.config import Config
 from backend.extensions import db, jwt, bcrypt
 from backend.routes import api
+from backend.models import User
 
 
 app = Flask(
@@ -14,16 +15,68 @@ app = Flask(
 
 app.config.from_object(Config)
 
+# Initialize extensions
+
 db.init_app(app)
 jwt.init_app(app)
 bcrypt.init_app(app)
 CORS(app)
 
+# Register routes
+
 app.register_blueprint(api)
 
+
+# ---------------- DATABASE SETUP ---------------- #
+
 with app.app_context():
+
     db.create_all()
 
+    # ---------------- AUTO CREATE DEMO USERS ---------------- #
+
+    admin_exists = User.query.filter_by(
+        email="admin@test.com"
+    ).first()
+
+    if not admin_exists:
+
+        hashed_password = bcrypt.generate_password_hash(
+            "123456"
+        ).decode("utf-8")
+
+        admin_user = User(
+            username="admin",
+            email="admin@test.com",
+            password=hashed_password,
+            role="admin"
+        )
+
+        db.session.add(admin_user)
+
+    member_exists = User.query.filter_by(
+        email="member@test.com"
+    ).first()
+
+    if not member_exists:
+
+        hashed_password = bcrypt.generate_password_hash(
+            "123456"
+        ).decode("utf-8")
+
+        member_user = User(
+            username="member",
+            email="member@test.com",
+            password=hashed_password,
+            role="member"
+        )
+
+        db.session.add(member_user)
+
+    db.session.commit()
+
+
+# ---------------- FRONTEND ROUTES ---------------- #
 
 @app.route("/")
 def home():
